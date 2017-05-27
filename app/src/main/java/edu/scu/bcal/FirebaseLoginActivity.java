e6f852dceb45ed5d44436d9b5b6ac9179884f492
beadlejack <beadlejack@me.com>
2017-05-27 16:05:39 -0700

    Sign in with Google running

--- app/src/main/java/edu/scu/bcal/FirebaseLoginActivity.java
+++ app/src/main/java/edu/scu/bcal/FirebaseLoginActivity.java
@@ -7,9 +7,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.ErrorCodes;
-import com.firebase.ui.auth.IdpResponse;
-import com.firebase.ui.auth.ResultCodes;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -40,15 +37,16 @@ public class FirebaseLoginActivity extends AppCompatActivity {
                 if (user != null) {
                     // User is signed in
                     Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    onSignInInitialize();
                 } else {
                     // User is signed out
                     Log.d(TAG, "onAuthStateChanged:signed_out");
                     startActivityForResult(
                             AuthUI.getInstance()
                                     .createSignInIntentBuilder()
-                                    .setIsSmartLockEnabled(!BuildConfig.DEBUG)
+                                    .setIsSmartLockEnabled(false)
                                     .setProviders(Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
-                                                                new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                                            new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
                                     .build(), RC_SIGN_IN);
                 }
             }
@@ -82,49 +80,12 @@ public class FirebaseLoginActivity extends AppCompatActivity {
         mAuth.addAuthStateListener(mAuthListener);
     }
 
-//    mAuth.createUserWithEmailAndPassword(email, password)
-//            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-//        @Override
-//        public void onComplete(@NonNull Task<AuthResult> task) {
-//            Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
-//
-//            // If sign in fails, display a message to the user. If sign in succeeds
-//            // the auth state listener will be notified and logic to handle the
-//            // signed in user can be handled in the listener.
-//            if (!task.isSuccessful()) {
-//                Toast.makeText(EmailPasswordActivity.this, R.string.auth_failed,
-//                        Toast.LENGTH_SHORT).show();
-//            }
-//
-//            // ...
-//        }
-//    });
-//
-//    mAuth.signInWithEmailAndPassword(email, password)
-//            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-//        @Override
-//        public void onComplete(@NonNull Task<AuthResult> task) {
-//            Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
-//
-//            // If sign in fails, display a message to the user. If sign in succeeds
-//            // the auth state listener will be notified and logic to handle the
-//            // signed in user can be handled in the listener.
-//            if (!task.isSuccessful()) {
-//                Log.w(TAG, "signInWithEmail:failed", task.getException());
-//                Toast.makeText(EmailPasswordActivity.this, R.string.auth_failed,
-//                        Toast.LENGTH_SHORT).show();
-//            }
-//
-//            // ...
-//        }
-//    });
-
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == ResultCodes.OK) {
-            IdpResponse idpResponse = IdpResponse.fromResultIntent(data);
-            startActivity(new Intent(this, WelcomeBackActivity.class)
-                    .putExtra("my_token", idpResponse.getIdpToken()));
-        }
+    protected void onSignInInitialize() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
+
+
 }
+
+
